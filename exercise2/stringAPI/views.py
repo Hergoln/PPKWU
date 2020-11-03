0b9266715fb7d9e28552fbf59efa4e3bb4fed514
Hergoln <juliusz.malka@o2.pl>
2020-11-03 15:11:59 +0100

    added histogram field to returned JSON response

--- exercise2/stringAPI/views.py
+++ exercise2/stringAPI/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
+from collections import Counter
 def stringAPI(request, string):
     data = {
         'lowerCase': sum(1 for c in string if c.islower()),
         'upperCase': sum(1 for c in string if c.isupper()),
         'digits': sum(1 for c in string if c.isdigit()),
-        'specialChar': sum(1 for c in string if not c.isalpha() and not c.isdigit())
+        'specialChar': sum(1 for c in string if not c.isalpha() and not c.isdigit()),
+        'histogram': Counter(string)
     }
     return JsonResponse(data)
