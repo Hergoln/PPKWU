b31ccb342c8516d97ca0e1a4b0b9d2e35d59601e
Hergoln <juliusz.malka@o2.pl>
2020-11-03 14:44:23 +0100

    implemented stringAPI method representing string endpoint

--- exercise2/stringAPI/views.py
+++ exercise2/stringAPI/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import JsonResponse
 def stringAPI(request, string):
-    return HttpResponse(string)
+    data = {
+        'lowerCase': sum(1 for c in string if c.islower()),
+        'upperCase': sum(1 for c in string if c.isupper()),
+        'digits': sum(1 for c in string if c.isdigit()),
+        'specialChar': sum(1 for c in string if not c.isalpha())
+    }
+    return JsonResponse(data)
