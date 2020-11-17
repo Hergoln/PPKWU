828b9b135343fb9666655fcede8180d36c6e2d2c
Hergoln <juliusz.malka@o2.pl>
2020-11-17 15:27:52 +0100

    endpoint now creates and returns content of example.ics file

--- exercise3/mobile_calendar_weeia/views.py
+++ exercise3/mobile_calendar_weeia/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from ics import Calendar, Event
 
 
 def calendar(request, dateString, timeString, eventTitle):
-    return HttpResponse(dateString + " " + timeString + ":\n" + eventTitle)
+    c = Calendar()
+    e = Event()
+    e.name = eventTitle
+    e.begin = dateString + ' ' + timeString
+    c.events.add(e)
+    with open('example.ics', 'w') as exemplary_file:
+        exemplary_file.writelines(c)
+    return HttpResponse(c)
