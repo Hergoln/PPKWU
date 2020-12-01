040f6c62a400e9d43c59e1860a07bf50b8e40162
Hergoln <juliusz.malka@o2.pl>
2020-12-01 14:54:11 +0100

    added make_stuff function for readability

--- exercise3/mobile_calendar_weeia/views.py
+++ exercise3/mobile_calendar_weeia/views.py
@@ -9,25 +9,34 @@ from bs4 import BeautifulSoup
 import unicodedata
 
 def calendar(request, month):
+	stuff = make_stuff(month)
+	return HttpResponse(stuff)
+
+def make_stuff(month):
 	year = datetime.datetime.now().year
 	URL= "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php"
-	PARAMS = {'rok':add_0_prefix_if_ness(year), 'miesiac':add_0_prefix_if_ness(month), 'lang':1}
+	month = add_0_prefix_if_ness(month)
+	# else:
+	# 	PARAMS = {'rok':year, 'miesiac':month, 'lang':1}
+	PARAMS = {'rok':year, 'miesiac':month, 'lang':1}
+	print(month)
 	r = requests.get(url=URL, params=PARAMS)
 	soup = BeautifulSoup(r.text, 'html.parser')
 
-	r = Calendar()
+	stuff = Calendar()
+
 	for num, action in enumerate(soup.find_all("a", class_="active")):
 		e = Event()
 
 		e.name = unicodedata.normalize('NFD', soup.find_all("div", class_="InnerBox")[num].string).encode('ascii', 'ignore').decode("utf-8")
-		print(str(year) + '-' + add_0_prefix_if_ness(month) + '-' + add_0_prefix_if_ness(action.string) + ' ' + '12:00')
-		e.begin = str(year) + '-' + add_0_prefix_if_ness(month) + '-' + add_0_prefix_if_ness(action.string) + ' ' + '12:00'
+		print(str(year) + '-' + month + '-' + add_0_prefix_if_ness(action.string) + ' ' + '12:00')
+		e.begin = str(year) + '-' + month + '-' + add_0_prefix_if_ness(action.string) + ' ' + '12:00'
 		
-		r.events.add(e)
-	with open('../githubPagesRoot/example.ics', 'w') as exemplary_file:
-	 	exemplary_file.writelines(r)
+		stuff.events.add(e)
 
-	return HttpResponse(r)
+	with open('../githubPagesRoot/example_march.ics', 'w') as exemplary_file:
+	 	exemplary_file.writelines(stuff)
+	return stuff
 
 def add_0_prefix_if_ness(s):
 	if int(str(s)) < 10:
