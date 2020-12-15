bfe4716b17f4ca020a9c27218fbac058ca303068
Hergoln <juliusz.malka@o2.pl>
2020-12-15 14:56:18 +0100

    now endpoint returns url

--- exercise4/vcard/views.py
+++ exercise4/vcard/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def vcard(request, string):
-    return HttpResponse(string) 
+    return HttpResponse(make_stuff(string))
+
+def make_stuff(searchQuery):
+    URL= "https://panoramafirm.pl/szukaj?k=" + '+'.join(searchQuery.split()) + "&l="
+    print(URL)
+    # r = requests.get(url=URL)
+    # soup = BeautifulSoup(r.text, 'html.parser')
+
+
+    # for num, action in enumerate(soup.find_all("a", class_="active")):
+    # 	e = Event()
+
+    # 	e.name = unicodedata.normalize('NFD', soup.find_all("div", class_="InnerBox")[num].string).encode('ascii', 'ignore').decode("utf-8")
+    # 	print(str(year) + '-' + month + '-' + add_0_prefix_if_ness(action.string) + ' ' + '12:00')
+    # 	e.begin = str(year) + '-' + month + '-' + add_0_prefix_if_ness(action.string) + ' ' + '12:00'
+
+    # 	stuff.events.add(e)
+
+    # with open('../githubPagesRoot/example.ics', 'w') as exemplary_file:
+    #  	exemplary_file.writelines(stuff)
+    return URL 
